0cf4bd824d8ff51807ce21f6cd3a943b6208895c
Christian Champaco <christian.champaco@alaska.gov>
2021-10-04 14:24:47 -0800

    Finished module

--- Pages/Pizza.cshtml.cs
+++ Pages/Pizza.cshtml.cs
@@ -9,8 +9,36 @@ namespace RazorPagesPizza.Pages
 {
     public class PizzaModel : PageModel
     {
+        public List<Pizza> pizzas;
+
+        [BindProperty]
+        public Pizza NewPizza { get; set; }
+
+        public string GlutenFreeText(Pizza pizza)
+        {
+            if (pizza.IsGlutenFree) return "Gluten Free";
+            return "Not Gluten Free";
+        }
+
         public void OnGet()
         {
+            pizzas = PizzaService.GetAll();
+        }
+
+        public IActionResult OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            PizzaService.Add (NewPizza);
+            return RedirectToAction("Get");
+        }
+
+        public IActionResult OnPostDelete(int id)
+        {
+            PizzaService.Delete (id);
+            return RedirectToAction("Get");
         }
     }
 }
